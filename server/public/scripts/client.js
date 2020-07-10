3494b75336d93ae2d3b69ddebc4f484a9f4ebccb
Ashleigh McGinnis <ashleighmcginnis@MacBook-Pro.local>
2020-07-10 13:30:18 -0500

    push values into table and array

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,7 @@
 console.log( 'js' );
 
+const newKoala =[]
+
 $( document ).ready( function(){
   console.log( 'JQ' );
   // Establish Click Listeners
@@ -7,24 +9,56 @@ $( document ).ready( function(){
   // load existing koalas on page load
   getKoalas();
 
+  
+
 }); // end doc ready
 
 function setupClickListeners() {
   $( '#addButton' ).on( 'click', function(){
     console.log( 'in addButton on click' );
+
     // get user input and put in an object
     // NOT WORKING YET :(
     // using a test object
     let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
+      name: $('#nameIn').val(),
+      age: $('#ageIn').val(),
+      gender: $('#genderIn').val(),
+      readyForTransfer: $('#readyForTransferIn').val(),
+      notes: $('#notesIn').val()
     };
+
+    $('#nameIn').val(''),
+    $('#ageIn').val(''),
+    $('#genderIn').val(''),
+    $('#readyForTransferIn').val(''),
+    $('#notesIn').val('')
+
+    newKoala.push(koalaToSend);
     // call saveKoala with the new obejct
     saveKoala( koalaToSend );
+
+    render();
   }); 
+}
+
+function render() {
+  $('#viewKoalas').empty();
+
+  for (let koalaToSend of newKoala) {
+    
+
+    $('#viewKoalas').append(`
+    <tr>
+            <td>${koalaToSend.name}</td>
+            <td>${koalaToSend.age}</td>
+            <td>${koalaToSend.gender}</td>
+            <td>${koalaToSend.readyForTransfer}</td>
+            <td>${koalaToSend.notes}</td>
+            
+            x
+          </tr>`);
+  }
 }
 
 function getKoalas(){
